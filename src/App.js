23d37d3e1416e1ea9ff5f9fd7219c085bf13351e
Vahan Barseghyan <51117466+vahanbarseghyan21@users.noreply.github.com>
2022-08-27 20:47:30 +0400

    Weather Api React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ContentWeather from './components/ContentWeather';
+
+function App() {
+    return (
+        <div className="app">
+            <ContentWeather />
+        </div>
+    );
+}
+
+export default App;
